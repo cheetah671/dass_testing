fddbe5afacb97a0ae1e2cc84204533ad72286f1a
cheetah671 <arnavagnihotri507@gmail.com>
2026-03-22 18:38:02 +0530

    Error 11: Require Full Group Ownership In all_owned_by

--- whitebox/moneypoly/moneypoly/moneypoly/property.py
+++ whitebox/moneypoly/moneypoly/moneypoly/property.py
@@ -80,7 +80,9 @@ class PropertyGroup:
         """Return True if every property in this group is owned by `player`."""
         if player is None:
             return False
-        return any(p.owner == player for p in self.properties)
+        if not self.properties:
+            return False
+        return all(p.owner == player for p in self.properties)
 
     def get_owner_counts(self):
         """Return a dict mapping each owner to how many properties they hold in this group."""
